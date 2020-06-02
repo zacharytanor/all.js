12c4551bd4eac0058db8201723c4d3779ed565ca
SwordFishTheChordedPlayer1 <64352813+SwordFishTheChordedPlayer1@users.noreply.github.com>
2020-06-02 10:33:38 +0200

    all.js
    all.js is an set of JavaScript API's/Frameworks For JavaScript.

---
+++ all.js
@@ -0,0 +1,26 @@
+var script1 = document.createElement("script");
+var script2 = document.createElement("script");
+var script3 = document.createElement("script");
+var script4 = document.createElement("script");
+var script5 = document.createElement("script");
+var script6 = document.createElement("script");
+var script7 = document.createElement("script");
+var script8 = document.createElement("script");
+
+script1.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
+script2.src = "https://ajax.googleapis.com/ajax/libs/d3js/5.15.1/d3.min.js";
+script3.src = "https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js";
+script4.src = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/p5.min.js";
+script5.src = "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js";
+script6.src = "https://cdnjs.cloudflare.com/ajax/libs/tone/14.7.3/Tone.js";
+script7.src = "https://cdnjs.cloudflare.com/ajax/libs/konva/6.0.0/konva.js";
+script8.src = "https://cdnjs.cloudflare.com/ajax/libs/pixi.js/5.2.4/pixi.min.js";
+
+document.head.appendChild(script1);
+document.head.appendChild(script2);
+document.head.appendChild(script3);
+document.head.appendChild(script4);
+document.head.appendChild(script5);
+document.head.appendChild(script6);
+document.head.appendChild(script7);
+document.head.appendChild(script8);
